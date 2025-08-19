1b8bad3cbeb1ddc3ea3929e4afce0c35a07baac9
J M Fahim Hasan <jmfahimhasan52@gmail.com>
2025-08-20 02:20:00 +0600

    7. print_ascii_value.cpp

---
+++ 7. print_ascii_value.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+
+{
+    char c;
+
+    cout << "Enter a character: ";
+
+
+
+
+
+    cin >> c;
+
+
+
+
+
+    cout << "The ASCII value of " << c << " is " << int(c) << endl;
+
+    return 0;
+
+
+    
+}
