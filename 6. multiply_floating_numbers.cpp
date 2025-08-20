dbbb59c172b299b41cd58e2b489987c2791f0dd1
J M Fahim Hasan <jmfahimhasan52@gmail.com>
2025-08-20 16:18:47 +0600

    6. multiply_floating_numbers.cpp

---
+++ 6. multiply_floating_numbers.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+    float num1, num2, Result;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+
+    Result = num1 * num2;
+
+    cout << "Result = " << Result << endl;
+
+    return 0;
+
+
+
+}
