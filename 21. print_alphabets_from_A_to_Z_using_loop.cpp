0fab790727b6742f24f764eedde88087c276edd5
J M Fahim Hasan <jmfahimhasan52@gmail.com>
2025-08-27 21:45:08 +0600

    21. print_alphabets_from_A_to_Z_using_loop.cpp

---
+++ 21. print_alphabets_from_A_to_Z_using_loop.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+
+
+
+
+int main()
+
+
+{
+    cout << "Alphabets from A to Z are:" << endl;
+    for (char ch = 'A'; ch <= 'Z'; ch++) 
+        cout << ch << " ";
+    
+
+    cout << endl;
+
+
+    return 0;
+
+    
+}
